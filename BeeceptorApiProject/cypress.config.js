4f6c4c51b7d1292984b2a529cf06c787e1efb655
Anki237 <ankitaverma14sept@gmail.com>
2024-07-21 22:12:22 +0530

    cypress api project adding

---
+++ BeeceptorApiProject/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+async function setupNodeEvents(on, config) {
+  // implement node event listeners here
+  return config;
+}
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents,
+    specPattern:"cypress/Integration/Test/*.js"
+  },
+});
